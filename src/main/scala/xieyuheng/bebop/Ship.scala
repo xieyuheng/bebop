3a3688684038ebff9f6894aec00d2d35a0dffa15
Xie Yuheng <xyheme@gmail.com>
2019-08-02 02:04:06 +0800

    up

--- src/main/scala/xieyuheng/bebop/Ship.scala
+++
@@ -1,9 +0,0 @@
-package xieyuheng.bebop
-
-trait Ship1[-T1, +R] {
-
-}
-
-trait Ship2[-T1, -T2, +R] {
-
-}
---
+++ src/main/scala/xieyuheng/bebop/Tran.scala
@@ -0,0 +1,9 @@
+package xieyuheng.bebop
+
+trait Tran1[-T1, +R] {
+
+}
+
+trait Tran2[-T1, -T2, +R] {
+
+}
